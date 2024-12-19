d132ec059740d450b1cffbc18b6495ec8b96be25
mesi260 <pablonicolassi@msmk.university>
2024-12-19 01:37:18 +0100

    Se separan las estaciones y las conexiones en otros archivos para organizar el nuevo main. Se mejora la funcionalidad, usando ecuaciones booleanas. Queda por establecer el tiempo real entre estaciones (de misma o diferente linea).

---
+++ conexiones.py
@@ -0,0 +1,119 @@
+conexiones = [
+    # Alonso Cano
+    ("Alonso Cano", "Avenida de América", True, "Línea 7"),
+
+    # Alonso Martínez
+    ("Alonso Martínez", "Argüelles", True, "Línea 4"),
+    ("Alonso Martínez", "Velázquez", True, "Línea 4"),
+    ("Alonso Martínez", "Callao", True, "Línea 5"),
+    ("Alonso Martínez", "Diego de León", True, "Línea 5"),
+    ("Alonso Martínez", "Tribunal", True, "Línea 10"),
+    ("Alonso Martínez", "Nuevos Ministerios", True, "Línea 10"),
+
+    # Avenida de América
+    ("Avenida de América", "Diego de León", True, "Línea 4"),
+    ("Avenida de América", "Alonso Cano", True, "Línea 7"),
+    ("Avenida de América", "Colombia", True, "Línea 9"),
+
+    # Argüelles
+    ("Argüelles", "Moncloa", True, "Línea 3"),
+    ("Argüelles", "Plaza de España", True, "Línea 3"),
+    ("Argüelles", "Alonso Martínez", True, "Línea 4"),
+    ("Argüelles", "Príncipe Pío", True, "Línea 6"),
+    ("Argüelles", "Moncloa", True, "Línea 6"),
+
+    # Callao
+    ("Callao", "Sol", True, "Línea 3"),
+    ("Callao", "Plaza de España", True, "Línea 3"),
+    ("Callao", "Ópera", True, "Línea 5"),
+    ("Callao", "Alonso Martínez", True, "Línea 5"),
+
+    # Chamartín
+    ("Chamartín", "Plaza de Castilla", True, "Línea 10"),
+    ("Chamartín", "Plaza de Castilla", True, "Línea 1"),
+
+    # Colombia
+    ("Colombia", "Nuevos Ministerios", True, "Línea 8"),
+    ("Colombia", "Avenida de América", True, "Línea 9"),
+    ("Colombia", "Plaza de Castilla", True, "Línea 9"),
+
+    # Cuatro Caminos
+    ("Cuatro Caminos", "Tribunal", True, "Línea 1"),
+    ("Cuatro Caminos", "Plaza de Castilla", True, "Línea 1"),
+    ("Cuatro Caminos", "Plaza de España", True, "Línea 2"),
+
+    # Diego de León
+    ("Diego de León", "Avenida de América", True, "Línea 4"),
+    ("Diego de León", "Goya", True, "Línea 4"),
+    ("Diego de León", "Alonso Martínez", True, "Línea 5"),
+    ("Diego de León", "Avenida de América", True, "Línea 6"),
+    ("Diego de León", "Manuel Becerra", True, "Línea 6"),
+
+    # Goya
+    ("Goya", "Manuel Becerra", True, "Línea 2"),
+    ("Goya", "Sol", True, "Línea 2"),
+    ("Goya", "Diego de León", True, "Línea 4"),
+    ("Goya", "Velázquez", True, "Línea 4"),
+
+    # Manuel Becerra
+    ("Manuel Becerra", "Goya", True, "Línea 2"),
+    ("Manuel Becerra", "Diego de León", True, "Línea 6"),
+
+    # Moncloa
+    ("Moncloa", "Argüelles", True, "Línea 3"),
+    ("Moncloa", "Argüelles", True, "Línea 6"),
+    ("Moncloa", "Cuatro Caminos", True, "Línea 6"),
+
+    # Nuevos Ministerios
+    ("Nuevos Ministerios", "Avenida de América", True, "Línea 6"),
+    ("Nuevos Ministerios", "Cuatro Caminos", True, "Línea 6"),
+    ("Nuevos Ministerios", "Colombia", True, "Línea 8"),
+    ("Nuevos Ministerios", "Alonso Martínez", True, "Línea 10"),
+    ("Nuevos Ministerios", "Santiago Bernabéu", True, "Línea 10"),
+
+    # Ópera
+    ("Ópera", "Sol", True, "Línea 2"),
+    ("Ópera", "Plaza de España", True, "Línea 2"),
+    ("Ópera", "Callao", True, "Línea 5"),
+    ("Ópera", "Príncipe Pío", True, "Ramal"),
+
+    # Plaza de Castilla
+    ("Plaza de Castilla", "Cuatro Caminos", True, "Línea 1"),
+    ("Plaza de Castilla", "Chamartín", True, "Línea 1"),
+    ("Plaza de Castilla", "Colombia", True, "Línea 9"),
+    ("Plaza de Castilla", "Chamartín", True, "Línea 10"),
+    ("Plaza de Castilla", "Santiago Bernabéu", True, "Línea 10"),
+
+    # Plaza de España
+    ("Plaza de España", "Cuatro Caminos", True, "Línea 2"),
+    ("Plaza de España", "Ópera", True, "Línea 2"),
+    ("Plaza de España", "Callao", True, "Línea 3"),
+    ("Plaza de España", "Argüelles", True, "Línea 3"),
+    ("Plaza de España", "Príncipe Pío", True, "Línea 10"),
+    ("Plaza de España", "Tribunal", True, "Línea 10"),
+
+    # Príncipe Pío
+    ("Príncipe Pío", "Ópera", True, "Ramal"),
+    ("Príncipe Pío", "Argüelles", True, "Línea 6"),
+    ("Príncipe Pío", "Plaza de España", True, "Línea 10"),
+
+    # Santiago Bernabéu
+    ("Santiago Bernabéu", "Plaza de Castilla", True, "Línea 10"),
+    ("Santiago Bernabéu", "Nuevos Ministerios", True, "Línea 10"),
+
+    # Sol
+    ("Sol", "Tribunal", True, "Línea 1"),
+    ("Sol", "Goya", True, "Línea 2"),
+    ("Sol", "Ópera", True, "Línea 2"),
+    ("Sol", "Callao", True, "Línea 3"),
+
+    # Tribunal
+    ("Tribunal", "Cuatro Caminos", True, "Línea 1"),
+    ("Tribunal", "Sol", True, "Línea 1"),
+    ("Tribunal", "Plaza de España", True, "Línea 10"),
+    ("Tribunal", "Alonso Martínez", True, "Línea 10"),
+
+    # Velázquez
+    ("Velázquez", "Goya", True, "Línea 4"),
+    ("Velázquez", "Alonso Martínez", True, "Línea 4")
+]
---
+++ estaciones.py
@@ -0,0 +1,7 @@
+# Lista de estaciones
+estaciones = [
+    "Alonso Cano", "Alonso Martínez", "Avenida de América", "Argüelles", "Callao", 
+    "Chamartín", "Colombia", "Cuatro Caminos", "Diego de León", "Goya", 
+    "Manuel Becerra", "Moncloa", "Nuevos Ministerios", "Ópera", "Plaza de Castilla", 
+    "Plaza de España", "Príncipe Pío", "Santiago Bernabéu", "Sol", "Tribunal", "Velázquez"
+]
